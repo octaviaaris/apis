524e29e758e37e03ddc130901ccce718ce829f03
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-20 11:42:35 -0700

    began lab

---
+++ secrets.sh
@@ -0,0 +1,7 @@
+# Secrets for your Eventbrite app
+#
+# This file should never go into github!
+
+export EVENTBRITE_TOKEN="Your-Credentials-Go-Here"
+export EVENTBRITE_KEY="Your-Credentials-Go-Here"
+export EVENTBRITE_SECRET="Your-Credentials-Go-Here"
